0eb85724eec10a53ba895ca8fe46ab058646e7e7
NIIDATE Kunitaka <nidate@core-note.jp>
2015-09-17 11:26:08 +0900

    Add vueify example

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   HelloVue: require('./lib/hello_vue'),
   HelloCoffee: require('./lib/hello_coffee.coffee'),
+  HelloVueify: require('./lib/hello_vueify.vue'),
   Items: require('./lib/items.coffee')
 };
